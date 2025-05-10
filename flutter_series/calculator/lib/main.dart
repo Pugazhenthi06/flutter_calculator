296ba41f1fc639050cf70ac32e6877f566417d12
Pugazhenthi06 <pugalp250@gmail.com>
2025-05-10 15:29:36 +0530

    Updated as stateful widget

--- flutter_series/calculator/lib/main.dart
+++ flutter_series/calculator/lib/main.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MainApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MainApp extends StatefulWidget {
+  
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
         body: Center(
           child: Text('Hello World!'),
